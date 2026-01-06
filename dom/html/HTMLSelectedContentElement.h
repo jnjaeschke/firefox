53f60b607922f5d267b5c507d2a803bcd4d83c4f
Jan-Niklas Jaeschke <jjaschke@mozilla.com>
2026-01-06 10:08:40 +0100

    fixup p3.1

--- dom/html/HTMLSelectedContentElement.h
+++ dom/html/HTMLSelectedContentElement.h
@@ -24,7 +24,7 @@ class HTMLSelectedContentElement final : public nsGenericHTMLElement {
   void SetDisabled(bool aDisabled) { mDisabled = aDisabled; }
   bool IsDisabled() const { return mDisabled; }
 
-  void ClearContent();
+  MOZ_CAN_RUN_SCRIPT void ClearContent();
 
   // Spec: selectedcontent post-connection steps
   // https://html.spec.whatwg.org/#the-selectedcontent-element
